fa0836132f9e916b2716b2ab798c7f97dbe847d6
wpaven <wpaven@yahoo.com>
2015-10-06 16:01:34 -0400

    removing solutions, moving to answers branch

--- src/com/marklogic/client/example/handson/unit03/Unit03_Exercises.java
+++ src/com/marklogic/client/example/handson/unit03/Unit03_Exercises.java
@@ -19,8 +19,6 @@ public class Unit03_Exercises {
 	//Task 1:  Write a query that finds pictures taken with "Apple" phones in "Hungary".
 	// Hint: you'll look in the "make" and "country" JSON properties
 	
-    //  qb.byExample({ country: 'Hungary', make: 'Apple' } 
-	
 	//Task 2: Delete the facets and tuples query options used in StructuredSearch.java
 	
 	public static void main(String[] args)
@@ -42,48 +40,4 @@ public class Unit03_Exercises {
 		runTask2(client);
 	}
 	
-	public static void runTask1(DatabaseClient client) 
-	throws IOException {
-		
-	    // create a manager for searching
-		QueryManager queryMgr = client.newQueryManager();
-
-		// create a raw query example
-		String rawJSONQuery =
-				"{"+
-                   "\"$query\": { \"make\": \"Apple\", \"country\": \"Hungary\" }"+
-                  "}";
-		
-		System.out.println(rawJSONQuery);
-		//create a handle
-		StringHandle rawHandle = new StringHandle();
-		rawHandle.withFormat(Format.JSON).set(rawJSONQuery);
-		
-		//create a raw query by example definition
-		RawQueryByExampleDefinition querydef =
-			    queryMgr.newRawQueryByExampleDefinition(rawHandle);
-		
-		// run the search
-		SearchHandle resultsHandle = 
-			    queryMgr.search(querydef, new SearchHandle());
-			
-	    Util.displayResults(resultsHandle);
-	    System.out.println("\nReturned results for runTask1()\n\n");
-	}
-	  
-		
-	
-	
-	public static void runTask2(DatabaseClient client) 
-	throws IOException {
-	  
-		QueryOptionsManager optionsMgr = client.newServerConfigManager().newQueryOptionsManager();
-		optionsMgr.deleteOptions("cities");
-		optionsMgr.deleteOptions("facets");
-		optionsMgr.deleteOptions("products");
-		optionsMgr.deleteOptions("tuples");
-		
-		System.out.println("\nDeleted options for runTask2()\n\n");
-		
-	}
 }
