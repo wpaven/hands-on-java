2d06aaa2851bc131f9e78773d6ae9bca33172920
wpaven <wpaven@yahoo.com>
2015-10-11 19:05:00 -0400

    cleaned up whitespace

--- src/com/marklogic/client/example/handson/unit04/Unit04_Exercises.java
+++ src/com/marklogic/client/example/handson/unit04/Unit04_Exercises.java
@@ -2,52 +2,52 @@ package com.marklogic.client.example.handson.unit04;
 
 public class Unit04_Exercises {
 	
-	//Task 1 
-	// The Geophoto application lets us provide a title for a picture. Add a title
-	// property with the value "Portland Head Lighthouse". This should be a child
-	// of the root element. When you're done, '/image/20140721_144421b.jpg.json'
-	// should look like this:
-	// {
-	//   "originalFilename":"../data/photos/20140721_144421b.jpg",
-	//   "filename":"20140721_144421b.jpg",
-	//   "binary":"/binary/20140721_144421b.jpg",
-	//   "make":"SAMSUNG",
-	//   "model":"SAMSUNG-SM-G900A",
-	//   "created":1405968260000,
-	//   "location":{
-    //	     "type":"Point",
-    //	     "coordinates":[43.6231, -70.208058],
-    //	     "city":"Cape Elizabeth",
-    //	     "state": "Maine",
-    //	     "country":"United States"
-	//   },
-	//   "title":"Portland Head Lighthouse"
-	// }
+//Task 1 
+// The Geophoto application lets us provide a title for a picture. Add a title
+// property with the value "Portland Head Lighthouse". This should be a child
+// of the root element. When you're done, '/image/20140721_144421b.jpg.json'
+// should look like this:
+// {
+//   "originalFilename":"../data/photos/20140721_144421b.jpg",
+//   "filename":"20140721_144421b.jpg",
+//   "binary":"/binary/20140721_144421b.jpg",
+//   "make":"SAMSUNG",
+//   "model":"SAMSUNG-SM-G900A",
+//   "created":1405968260000,
+//   "location":{
+//	     "type":"Point",
+//	     "coordinates":[43.6231, -70.208058],
+//	     "city":"Cape Elizabeth",
+//	     "state": "Maine",
+//	     "country":"United States"
+//   },
+//   "title":"Portland Head Lighthouse"
+// }
 	
-	//Task 2
-	// The image we inserted above describes the camera used to
-	// take the picture. In this exercise, we'll change the model. In the original
-	// document, the model is "SAMSUNG-SM-G900A". Change it to "Galaxy S5".
-	//
-	// {
-    //  "originalFilename":"../data/photos/20140721_144421b.jpg",
-    //  "filename":"20140721_144421b.jpg",
-    //  "binary":"/binary/20140721_144421b.jpg",
-    //  "make":"SAMSUNG",
-    //  "model":"Galaxy S5",
-    //  "created":1405968260000,
-    //  "location":{
-    //    "type":"Point",
-    //    "coordinates":[43.6231, -70.208058],
-    //    "city":"Cape Elizabeth",
-    //    "state": "Maine",
-    //    "country":"United States"
-    //  },
-    //  "title":"Portland Head Lighthouse"
-    //}
+//Task 2
+// The image we inserted above describes the camera used to
+// take the picture. In this exercise, we'll change the model. In the original
+// document, the model is "SAMSUNG-SM-G900A". Change it to "Galaxy S5".
+//
+// {
+//  "originalFilename":"../data/photos/20140721_144421b.jpg",
+//  "filename":"20140721_144421b.jpg",
+//  "binary":"/binary/20140721_144421b.jpg",
+//  "make":"SAMSUNG",
+//  "model":"Galaxy S5",
+//  "created":1405968260000,
+//  "location":{
+//    "type":"Point",
+//    "coordinates":[43.6231, -70.208058],
+//    "city":"Cape Elizabeth",
+//    "state": "Maine",
+//    "country":"United States"
+//  },
+//  "title":"Portland Head Lighthouse"
+//}
 
-	//Task 3
-	// In AddCollection, '/image/20140721_144421b.jpg.json' was added
-	// to the 'phone' collection. Now remove it from that collection
+//Task 3
+// In AddCollection, '/image/20140721_144421b.jpg.json' was added
+// to the 'phone' collection. Now remove it from that collection
 
 }
