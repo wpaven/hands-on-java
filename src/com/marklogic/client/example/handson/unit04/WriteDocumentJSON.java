8d8d657799b8bf349b92c5a3d7f52ff8b2ca7610
wpaven <wpaven@yahoo.com>
2015-10-11 18:39:38 -0400

    adding write examples and unit test

--- src/com/marklogic/client/example/handson/unit04/WriteDocumentJSON.java
+++ src/com/marklogic/client/example/handson/unit04/WriteDocumentJSON.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 
 import com.marklogic.client.DatabaseClient;
 import com.marklogic.client.DatabaseClientFactory;
+import com.marklogic.client.document.JSONDocumentManager;
 import com.marklogic.client.document.XMLDocumentManager;
 import com.marklogic.client.example.handson.Util;
 import com.marklogic.client.example.handson.Util.ExampleProperties;
@@ -26,39 +27,39 @@ public class WriteDocumentJSON {
 
 			// create the client
 			DatabaseClient client = DatabaseClientFactory.newClient(
-					props.host, props.port, props.writerUser, props.writerPassword,
+					props.host, props.port, props.adminUser, props.adminPassword,
 					props.authType);
 
 			// use either shortcut or strong typed IO
 			runShortcut(client);
 			runStrongTyped(client);
-
-			//tearDownExample(client);
+			tearDownExample(client);
 
 			// release the client
 			client.release();
 		}
 		public static void runShortcut(DatabaseClient client) throws IOException {
-			String filename = "flipper.xml";
+			String filename = "20140721_144421b.jpg.json";
 
 			// acquire the content
 			InputStream docStream = Util.openStream("data"+File.separator+filename);
 			if (docStream == null)
 				throw new IOException("Could not read document example");
 
-			// create a manager for XML documents
-			XMLDocumentManager docMgr = client.newXMLDocumentManager();
+			// create a manager for JSON documents
+			JSONDocumentManager docMgr = client.newJSONDocumentManager();
 
 			// create an identifier for the document
-			String docId = "/example/"+filename;
+			String docId = "/image/"+filename;
 
 			// write the document content
 			docMgr.writeAs(docId, docStream);
 
-			System.out.println("(Shortcut) Wrote /example/"+filename+" content");
+			System.out.println("(Shortcut) Wrote /image/"+filename+" content");
 		}
+		
 		public static void runStrongTyped(DatabaseClient client) throws IOException {
-			String filename = "flipper.xml";
+			String filename = "20140721_144421b.jpg.json";
 
 			// acquire the content
 			InputStream docStream = Util.openStream("data"+File.separator+filename);
@@ -66,10 +67,10 @@ public class WriteDocumentJSON {
 				throw new IOException("Could not read document example");
 
 			// create a manager for XML documents
-			XMLDocumentManager docMgr = client.newXMLDocumentManager();
+			JSONDocumentManager docMgr = client.newJSONDocumentManager();
 
 			// create an identifier for the document
-			String docId = "/example/"+filename;
+			String docId = "/image/"+filename;
 
 			// create a handle on the content
 			InputStreamHandle handle = new InputStreamHandle();
@@ -79,6 +80,17 @@ public class WriteDocumentJSON {
 			docMgr.write(docId, handle);
 
 			System.out.println("(Strong Typed) Wrote /example/"+filename+" content");
+		}
+		
+		// clean up by deleting the document that the example wrote
+		public static void tearDownExample(DatabaseClient client) {
+			JSONDocumentManager docMgr =  client.newJSONDocumentManager();
+
+			String docId = "/image/20140721_144421b.jpg.json";
+
+			docMgr.delete(docId);
+			
+			System.out.println("Deleted "+ docId);
 		}
 
 
---
+++ src/com/marklogic/client/example/handson/unit04/WriteMultiDocument.java
@@ -0,0 +1,88 @@
+package com.marklogic.client.example.handson.unit04;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.marklogic.client.DatabaseClient;
+import com.marklogic.client.DatabaseClientFactory;
+import com.marklogic.client.document.DocumentWriteSet;
+import com.marklogic.client.document.GenericDocumentManager;
+import com.marklogic.client.document.JSONDocumentManager;
+import com.marklogic.client.example.handson.Util;
+import com.marklogic.client.example.handson.Util.ExampleProperties;
+import com.marklogic.client.example.handson.unit01.StreamReadWrite;
+import com.marklogic.client.io.FileHandle;
+import com.marklogic.client.io.InputStreamHandle;
+import com.marklogic.client.io.JacksonHandle;
+
+public class WriteMultiDocument {
+	
+	public static void main(String[] args) throws IOException {
+		run(Util.loadProperties());
+	}
+
+	public static void run(ExampleProperties props) throws IOException {
+		System.out.println("example: "+WriteDocumentJSON.class.getName());
+
+		// create the client
+		DatabaseClient client = DatabaseClientFactory.newClient(
+				props.host, props.port, props.adminUser, props.adminPassword,
+				props.authType);
+
+		// use either shortcut or strong typed IO
+		loadContent(client);
+		tearDownExample(client);
+
+		// release the client
+		client.release();
+	}
+
+	
+	public static void loadContent(DatabaseClient client) throws IOException {
+		String jsonFile = "20140721_144421b.jpg.json";
+		String binaryFile = "20140721_144421b.jpg";
+
+		// acquire the content
+		InputStream jsonContent = Util.openStream("data"+File.separator+jsonFile);
+		if (jsonContent == null)
+			throw new IOException("Could not read json document example");
+		
+		 FileHandle binaryContent = 
+		          new FileHandle().with(new File(WriteMultiDocument.class.getProtectionDomain().getCodeSource().getLocation().getPath()
+	                         + File.separator +"data"
+	                         + File.separator + binaryFile)).withMimetype("image/jpeg");
+
+		// create a generic manager for mixed type documents
+		GenericDocumentManager docMgr = client.newDocumentManager();
+
+		// create identifiers for the documents
+		String jsonDocId = "/image/"+jsonFile;
+		String binaryDocId = "/binary/"+binaryFile;
+
+		// create a handle on the content
+		InputStreamHandle handle = new InputStreamHandle();
+		handle.set(jsonContent);
+
+	    DocumentWriteSet batch = docMgr.newWriteSet();
+	    batch.add(jsonDocId, handle);
+	    batch.add(binaryDocId, binaryContent);
+	    docMgr.write(batch);
+
+		System.out.println("Loaded files: \n "+ jsonDocId +"\n"+binaryDocId);
+	}
+	
+	// clean up by deleting the document that the example wrote
+	public static void tearDownExample(DatabaseClient client) {
+		GenericDocumentManager docMgr =  client.newDocumentManager();
+
+		String jsonDocId = "/image/20140721_144421b.jpg.json";
+		String binaryDocId = "/binary/20140721_144421b.jpg";
+
+		docMgr.delete(jsonDocId,binaryDocId);
+		
+		System.out.println("Deleted "+ jsonDocId + " and "+ binaryDocId);
+	}
+
+
+}
